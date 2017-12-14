b6b39a3b96407aaaf667d8172cdf6d76b147aeca
pmagdeline <preethamagdeline@gmail.com>
2017-12-14 16:10:28 +1100

    Change Program.cs

--- TechnicalTask/Program.cs
+++ TechnicalTask/Program.cs
@@ -37,92 +37,122 @@ namespace TechnicalTask
         }
         static void RecordDataToFile(string fpath)
         {
-            //Variable to assign the record size of the file
-            int recordSize = 0;
-            Console.Write("Enter Record size: ");
-            recordSize = Int32.Parse(Console.ReadLine());
-            //Array to store the numerical values
-            float[] x = new float[recordSize];
-            Console.Write("Record " + recordSize + " numerical values: ");
-            //Reading data to the float srray
-            for (int i = 0; i < recordSize; i++)
+            try
             {
-                x[i] = float.Parse(Console.ReadLine());
+                //Variable to assign the record size of the file
+                int recordSize = 0;
+                Console.Write("Enter Record size : ");
+                recordSize = Int32.Parse(Console.ReadLine());
+                //Array to store the numerical values
+                float[] x = new float[recordSize];
+                Console.Write("Record " + recordSize + " numerical values: ");
+                //Reading data to the float srray
+                for (int i = 0; i < recordSize; i++)
+                {
+                    x[i] = float.Parse(Console.ReadLine());
+                }
+                //Pass the file path and file name to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter(filePath, true);
+                for (int i = 0; i < x.Length; i++)
+                {
+                    sw.WriteLine(x[i]);
+                }
+                //Clear the buffer
+                sw.Flush();
+                //Close the file
+                sw.Close();
+
             }
-            //Pass the file path and file name to the StreamWriter Constructor
-            StreamWriter sw = new StreamWriter(filePath, true);
-            for (int i = 0; i < x.Length; i++)
+            catch (Exception e)
             {
-                sw.WriteLine(x[i]);
+                Console.WriteLine("Exception: " + e.Message);
+
             }
-            //Clear the buffer
-            sw.Flush();
-            //Close the file
-            sw.Close();
 
         }
         static void ReadDataFromFile(string fpath)
         {
-            // Open the file to read from.
-            using (StreamReader sr = File.OpenText(filePath))
+            try
             {
-                string s = "";
-                Console.WriteLine("------------------------------------------------");
-                while ((s = sr.ReadLine()) != null)
+                // Open the file to read from.
+                using (StreamReader sr = File.OpenText(filePath))
                 {
-                    string[] lines = File.ReadAllLines(filePath);
-                    Console.WriteLine(">> TechnicalTask.exe record numeric value >> " + s);
-                    
-                }
+                    string s = "";
+                    Console.WriteLine("------------------------------------------------");
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        string[] lines = File.ReadAllLines(filePath);
+                        Console.WriteLine(">> TechnicalTask.exe record numeric value >> " + s);
 
+                    }
+
+                }
+                Console.WriteLine("------------------------------------------------");
             }
-            Console.WriteLine("------------------------------------------------");
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("[Text File Missing] {0}", e);
+                throw new FileNotFoundException(@"[Technicaltask.txt not in c:\ directory]", e);
+            }
+
+
         }
         static void DisplayFileSummary(string fpath)
         {
-            //Declare variables to store maximum, minimum, total and average values
-            float max = 0;
-            float min = 999999;
-            float score = 0;
-            int count;
-            float totalVal = 0;
-            float avgVal = 0;
-            // Open the file to read from.
-            using (StreamReader sr = File.OpenText(filePath))
+            try
             {
-                string s = "";
-                count = 0;
-                Console.WriteLine("\r\n\tTechnicalTask.txt Summary\r\n");
-                Console.WriteLine("-----------------------------------------");
-                while ((s = sr.ReadLine()) != null)
+                //Declare variables to store maximum, minimum, total and average values
+                float max = 0;
+                float min = 999999;
+                float score = 0;
+                int count;
+                float totalVal = 0;
+                float avgVal = 0;
+                // Open the file to read from.
+                using (StreamReader sr = File.OpenText(filePath))
                 {
-                    string[] lines = File.ReadAllLines(filePath);
-                    count++;
-                    foreach (string line in lines)
+                    string s = "";
+                    count = 0;
+                    Console.WriteLine("\r\n\tTechnicalTask.txt Summary\r\n");
+                    Console.WriteLine("-----------------------------------------");
+                    while ((s = sr.ReadLine()) != null)
                     {
-                        if (float.TryParse(line, out score))
+                        string[] lines = File.ReadAllLines(filePath);
+                        count++;
+                        totalVal = 0;
+                        foreach (string line in lines)
                         {
-                            if (score > max)
-                                max = score;
-                            if (score < min)
-                                min = score;
+                            if (float.TryParse(line, out score))
+                            {
+                                if (score > max)
+                                    max = score;
+                                if (score < min)
+                                    min = score;
+                            }
+                            totalVal += float.Parse(line);
+
                         }
-                        totalVal += float.Parse(line);
-                       
+
                     }
-
-
+                    avgVal = (totalVal / count);
+                    Console.WriteLine("|" + "\t" + "# of items" + "\t" + "|" + "\t" + count + "\t" + "|");
+                    Console.WriteLine("|" + "\t" + "Min value" + "\t" + "|" + "\t" + min + "\t" + "|");
+                    Console.WriteLine("|" + "\t" + "Max value" + "\t" + "|" + "\t" + max + "\t" + "|");
+                    Console.WriteLine("|" + "\t" + "Average value" + "\t" + "|" + "\t" + avgVal.ToString("0.00") + "\t" + "|");
+                    Console.WriteLine("-----------------------------------------");
                 }
-                avgVal = totalVal / count;
-                Console.WriteLine("|" + "\t" + "# of items" + "\t" + "|" + "\t" + count + "\t" + "|");
-                Console.WriteLine("|" + "\t" + "Min value" + "\t" + "|" + "\t" + min + "\t" + "|");
-                Console.WriteLine("|" + "\t" + "Max value" + "\t" + "|" + "\t" + max + "\t" + "|");
-                Console.WriteLine("|" + "\t" + "Average value" + "\t" + "|" + "\t" + avgVal + "\t" + "|");
-                Console.WriteLine("-----------------------------------------");
+
 
             }
-        }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("[Text File Missing] {0}", e);
+                throw new FileNotFoundException(@"[TechnicalTask.txt not in c:\ directory]", e);
+            }
 
 
         }
+
+
     }
+}
